99169215a7fbf6124e12fda87243c06059ae03e3
Pavel Vanacek <pavelvanacek@gmail.com>
2021-05-17 10:03:56 +0200

    I add class Customer

--- main.py
+++ main.py
@@ -42,11 +42,47 @@ class Basket:
     def remove(self, item):
         self.__items.remove(item)
 
-# class Customer:
-#
-#     def __init__(self,):
-#         self.name = name
-#         self.bi
+class Customer:
+
+    def __init__(self, name, birthdate):
+        self.name = name
+        self.birthdate = birthdate
+        self.basket = basket()
+
+    @property
+    def basket(self):
+        return _basket
+
+    @basket.setter
+    def basket(self, new_basket):
+        self._basket = new_basket
+
+    @property
+    def age(self):
+        return _calculate_age()
+
+    def _calculate_age(self):
+        diff = date.today() - self.birthdate
+        return round(diff.days / 365, 1)
+
+    def add_to_basket(self, item):
+        if isinstance(item, Item):
+            if item.is_allowed(self.age):
+                self._basket.add(item)
+            else:
+                raise ValueError(f"Cannot purchase '{item.name}', you haven't reach the age limit!")
+        else:
+            raise TypeError(f"Can add only instances of class 'Item'")
+
+    def remove_to_basket(self, item):
+        if isinstance(item, Item):
+            self._basket.remove(item)
+        else:
+            raise TypeError(f"Can remove only instances of class 'Item'")
+
+
+
+
 
 
 
